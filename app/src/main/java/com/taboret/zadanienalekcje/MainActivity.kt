71f8a6c4b81a8c77af504182e522eb029c2a12c1
student-14 <terleckawika123@gmail.com>
2022-10-21 15:09:38 +0200

    ce

--- app/src/main/java/com/taboret/zadanienalekcje/MainActivity.kt
+++ app/src/main/java/com/taboret/zadanienalekcje/MainActivity.kt
@@ -2,10 +2,18 @@ package com.taboret.zadanienalekcje
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val dopisz = findViewById<Button>(R.id.dopisz)
+        val przywroc = findViewById<Button>()
+
     }
 }
