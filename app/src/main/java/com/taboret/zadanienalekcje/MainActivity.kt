e74095c226d0de1ed4d0e3fb8719bafc974d41b2
student-14 <terleckawika123@gmail.com>
2022-10-21 15:14:05 +0200

    23

--- app/src/main/java/com/taboret/zadanienalekcje/MainActivity.kt
+++ app/src/main/java/com/taboret/zadanienalekcje/MainActivity.kt
@@ -12,8 +12,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val dopisz = findViewById<Button>(R.id.dopisz)
-        val przywroc = findViewById<Button>()
-
+        val dopisz = findViewById<Button>(R.id.button)
+        val przywroc = findViewById<Button>(R.id.button2)
+        val tekst = findViewById<TextView>(R.id.textView)
+        val wpisz = findViewById<EditText>(R.id.edittext)
+ dopisz.setOnClickListener {
+     tekst.text = tekst.text.toString() + wpisz.text
+ }
+        przywroc.setOnClickListener {
+            tekst.text = "Julian Stajnke"
+        }
     }
 }
